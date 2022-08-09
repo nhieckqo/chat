92c834785a31f9dce66c3070cb52d1cc6447196c
nhieckqo <nhieckqo9@yahoo.com>
2022-08-09 22:55:10 +0800

    refactor front-end codes

--- chat/views.py
+++ chat/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html', {})
+    return render(request, 'chat/index.html', {})
 
 
 def room(request, room_name):
-    return render(request, 'chatroom.html',{
+    return render(request, 'chat/chatroom.html',{
         'room_name':room_name,
     })
